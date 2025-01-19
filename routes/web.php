b3925b2f10e0a42655e064c402be28fb9a025d55
lucascastellani <lucas100castellani@gmail.com>
2025-01-19 15:59:29 -0300

    fix: vite

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@ Route::get('/whiteboard/{any}', function () {
     return view('whiteboard');
 })->where('any', '.*');
 
-Route::post('api/stroke', [DrawingController::class, 'saveStroke']);
-Route::get('api/whiteboard/{pass}', [DrawingController::class, 'getWhiteboardState']);
-Route::post('api/clear-whiteboard', [DrawingController::class, 'clearWhiteboard']);
+Route::post('/api/stroke', [DrawingController::class, 'saveStroke']);
+Route::get('/api/whiteboard/{pass}', [DrawingController::class, 'getWhiteboardState']);
+Route::post('/api/clear-whiteboard', [DrawingController::class, 'clearWhiteboard']);
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        laravel({
+            input: [
+                path.resolve(__dirname, 'resources/js/app.js'), // ajuste o caminho se necessário
+                path.resolve(__dirname, 'resources/css/app.css') // se você tiver arquivos CSS
+            ],
+        }),
+    ],
 });
